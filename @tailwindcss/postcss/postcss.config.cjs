4d74daca80c099a0645888eff20822fe867ac7cc
Hunter <i.am.hunter.777.ih@gmail.com>
2025-05-31 07:54:34 +0300

    upd: move  postcss.config.cjs to @tailwindcss/postcss folder

--- @tailwindcss/postcss/postcss.config.cjs
+++ @tailwindcss/postcss/postcss.config.cjs
@@ -1,9 +1,11 @@
 const tailwindcss = require('tailwindcss');
 const autoprefixer = require('autoprefixer');
+// '@tailwindcss/postcss' - это плагин для PostCSS, который позволяет использовать Tailwind CSS в PostCSS
 
 module.exports = {
   plugins: {
     tailwindcss: {}, // tailwindcss(), если передать опции
     autoprefixer: {},
+    '@tailwindcss/postcss': {},
   },
 };
