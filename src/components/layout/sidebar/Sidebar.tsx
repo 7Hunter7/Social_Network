dbe8e67ceb8b0eb928a267bad9078372dfa26295
Hunter <i.am.hunter.777.ih@gmail.com>
2025-06-01 16:37:57 +0300

    upd(Sidebar): added using map() for <Link> using MENU

--- src/components/layout/sidebar/Sidebar.tsx
+++ src/components/layout/sidebar/Sidebar.tsx
@@ -1,26 +1,18 @@
-import { Phone, User2, MessagesSquare, Cog } from 'lucide-react';
+'use client'
 import Image from 'next/image';
 import Link from 'next/link';
 import styles from './Sidebar.module.scss';
+import { MENU } from './sidebar.data';
 
 export default function Sidebar() {
   return (
     <aside className={styles.sidebar}>
       <Image src='/logo.svg' priority alt='logo' width={50} height={50} />
-      <div>
-        <Link href='/friends'>
-          <User2 size={27}/>
+      {MENU.map((item) => (
+        <Link href={item.url} key={item.url}>
+          <item.icon size={27} />
         </Link>
-        <Link href='/call'>
-          <Phone size={27}/>
-        </Link>
-        <Link href='/chats'>
-          <MessagesSquare size={27} />
-        </Link>
-        <Link href='/settings'>
-          <Cog size={27}/>
-        </Link>
-      </div>
+      ))}
     </aside>
   );
 }
