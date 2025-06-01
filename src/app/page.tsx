fdc4b8635b2b27909e0b21440de26e61e26bd16f
Hunter <i.am.hunter.777.ih@gmail.com>
2025-06-01 17:15:00 +0300

    upd(ChatsPage):  added import and using CurrentUser, ChatsList and Chat

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,7 @@
+import CurrentUser from '@/components/(chats)/CurrentUser';
+import ChatsList from '@/components/(chats)/ChatsList';
+import Chat from '@/components/(chats)/Chat';
+
 export default function ChatsPage() {
   return (
     <div >
