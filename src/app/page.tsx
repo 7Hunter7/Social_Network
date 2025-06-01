7eb78139f6145d1e56415037129366ecf932410d
Hunter <i.am.hunter.777.ih@gmail.com>
2025-06-01 17:23:40 +0300

    upd(ChatsPage): added using styles from module

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 import CurrentUser from '@/components/(chats)/CurrentUser';
 import ChatsList from '@/components/(chats)/ChatsList';
 import Chat from '@/components/(chats)/Chat';
+import styles from './page.module.scss';
 
 export default function ChatsPage() {
   return (
-    <div >
+    <div className={styles.chats}>
       <div>
         <CurrentUser />
         <ChatsList />
