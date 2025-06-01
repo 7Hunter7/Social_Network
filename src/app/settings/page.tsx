39c550c24eee7a896edbed05259332ef12865389
Hunter <i.am.hunter.777.ih@gmail.com>
2025-06-01 16:17:02 +0300

    upd: created new page "SettingsPage"

---
+++ src/app/settings/page.tsx
@@ -0,0 +1,16 @@
+import type { Metadata } from "next";
+
+
+export const metadata: Metadata = {
+  title: 'Settings',
+  description: 'Your Settings',
+
+}
+
+export default function SettingsPage() {
+  return (
+    <main>
+      <h1>Settings page</h1>
+    </main>
+  );
+}
