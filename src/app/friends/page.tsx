7ec3ae789f2cc9452401210e7a3c6deba1d37a78
Hunter <i.am.hunter.777.ih@gmail.com>
2025-06-01 16:14:55 +0300

    upd: created new page "FriendsPage"

---
+++ src/app/friends/page.tsx
@@ -0,0 +1,16 @@
+import type { Metadata } from "next";
+
+
+export const metadata: Metadata = {
+  title: 'Friends',
+  description: 'Your Friends List',
+
+}
+
+export default function FriendsPage() {
+  return (
+    <main>
+      <h1>Friends page</h1>
+    </main>
+  );
+}
