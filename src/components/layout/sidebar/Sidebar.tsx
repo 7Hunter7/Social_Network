682d3c560e9a1e70409b0c3bf66136cbe6f1080a
Hunter <i.am.hunter.777.ih@gmail.com>
2025-06-01 16:58:09 +0300

    upd(Sidebar): added using getServerPathName() and 'cn' from 'clsx'

--- src/components/layout/sidebar/Sidebar.tsx
+++ src/components/layout/sidebar/Sidebar.tsx
@@ -3,13 +3,18 @@ import Image from 'next/image';
 import Link from 'next/link';
 import styles from './Sidebar.module.scss';
 import { MENU } from './sidebar.data';
+import { getServerPathName } from '@/server-actions/get-path-name';
+import { cn } from 'clsx';
+
 
 export default function Sidebar() {
+  const pathname = getServerPathName();
+
   return (
     <aside className={styles.sidebar}>
       <Image src='/logo.svg' priority alt='logo' width={50} height={50} />
       {MENU.map((item) => (
-        <Link href={item.url} key={item.url}>
+        <Link href={item.url} key={item.url} className={cn({[styles.active]: pathname === item.url})}>
           <item.icon size={27} />
         </Link>
       ))}
